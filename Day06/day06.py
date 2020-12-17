8d35d0da85230a75680f2340200d7593585ced20
Paula Tennent <pjtennent@gmail.com>
2020-12-17 14:30:36 +1100

    cleaned up day06

--- Day06/day06.py
+++ Day06/day06.py
@@ -1,5 +1,43 @@
 import sys
-import re 
+import re
+
+# first subquestion
+def a():
+    # add into a set
+    total = 0
+    for group in groups:
+        questions = set()
+        for char in group:
+            #print(char)
+            if (char != "\n"):
+                questions.add(char)
+
+        # count set
+        total += len(questions)
+
+    return total
+
+# second subquestion
+def b():
+
+# add into a set
+    total = 0
+    for group in groups:
+        questions = {}
+        people = group.split("\n")
+        for person in people:
+            for char in person:
+                if (char != "\n"):
+                    if char in questions:
+                        questions[char] += 1
+                    else:
+                        questions[char] = 1 
+
+        # count set
+        for item in questions:
+            if (questions[item] == len(people)):
+                total += 1
+    return total
 
 # read in inputs
 wholeInput = sys.stdin.read()
@@ -7,18 +45,5 @@ wholeInput = sys.stdin.read()
 # sepeate by double \n
 groups = wholeInput.split("\n\n")
 
-
-# add into a set
-total = 0
-for group in groups:
-    questions = set()
-    for char in group:
-        #print(char)
-        if (char != "\n"):
-            questions.add(char)
-
-    # count set
-    total += len(questions)
-
-# add to total
-print(total)
+print(a())
+print(b())
