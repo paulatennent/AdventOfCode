a81d167825a4d0d41cec88a5c23c979a3a788155
Paula Tennent <pjtennent@gmail.com>
2020-12-22 14:49:24 +1100

    finished day 08

--- Day08/day08.py
+++ Day08/day08.py
@@ -2,15 +2,13 @@ import sys
 wholeInput = sys.stdin.read()
 
 def a():
-    seen = set()
-
     acc = 0
     curr = 0
-    while (curr != len(lines)):
+    while (curr < len(lines)):
 
         if curr in seen:
             return acc
-        seen.add(curr)
+        seen[curr] = acc
 
         line = lines[curr].split(" ")
 
@@ -29,10 +27,89 @@ def a():
                 curr += int(jmp)
             else:
                 curr -= int(jmp)
-            
+
+def test(curr2, acc2, ins):
+
+    # change the instruction from nop to jmp OR jmp to nop
+    line = lines[curr2].split(" ")
+    if (ins == "nop"):
+        jmp = line[1][1:]
+        if (line[1][0] == '+'):
+            curr2 += int(jmp)
+        else:
+            curr2 -= int(jmp)
+
+    if (ins == "jmp"):
+        curr2 += 1
+
+    # continue the program
+    while (curr2 < len(lines)):
+
+        # if we find something that has been seen, return false
+        if curr2 in seen:
+            return False
+        seen[curr2] = acc2
+
+        line = lines[curr2].split(" ")
+        if (line[0] == "nop"):
+            curr2 += 1
+        elif (line[0] == "acc"):
+            inc = line[1][1:]
+            curr2 += 1
+            if (line[1][0] == '+'):
+                acc2 += int(inc)
+            else:
+                acc2 -= int(inc)
+        elif (line[0] == "jmp"):
+            jmp = line[1][1:]
+            if (line[1][0] == '+'):
+                curr2 += int(jmp)
+            else:
+                curr2 -= int(jmp)
+
+    # if we got to the end of the prgram, we can return the accumliator
+    return acc2
+    
 
 def b():
-    pass
+    # make sure a() has be called at least once
+    acc = 0
+    curr = 0
+    while (curr < len(lines)):
+
+        # this hopefully shouldnt happen
+        if curr in seen:
+            return False
+
+        line = lines[curr].split(" ")
+
+        if (line[0] == "nop"):
+
+            ans = test(curr, acc, "nop")
+            if (ans != False):
+                return ans
+
+            curr += 1
+        elif (line[0] == "acc"):
+            inc = line[1][1:]
+            curr += 1
+            if (line[1][0] == '+'):
+                acc += int(inc)
+            else:
+                acc -= int(inc)
+        elif (line[0] == "jmp"):
+
+            ans = test(curr, acc, "jmp")
+            if (ans != False):
+                return ans
+
+            jmp = line[1][1:]
+            if (line[1][0] == '+'):
+                curr += int(jmp)
+            else:
+                curr -= int(jmp)
 
 lines = wholeInput.split("\n")
+seen = {}
 print(a())
+print(b())
